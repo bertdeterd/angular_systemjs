8054d7ceceb86a2c67d170c2ef1710379175abf2
Deterd <Bert.Deterd2@stedin.net>
2016-06-22 22:19:30 +0200

    startpage

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,7 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 @Component({
   selector: 'my-app', 
   directives: [ROUTER_DIRECTIVES],
+  styles: [`.fullheight { height:100%;}` ],
   template: require('./app.component.html')
 })
 
